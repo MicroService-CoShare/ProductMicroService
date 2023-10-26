0a4d63855898d670a2466c2fac6819be520ae2f6
Mouhib-Saleh <mouhib.saleh20@gmail.com>
2023-10-26 15:38:05 +0100

    fixed keycloak jwt iss

--- src/main/java/tn/esprit/cosharemsmouhib/controllers/ProductRestAPI.java
+++ src/main/java/tn/esprit/cosharemsmouhib/controllers/ProductRestAPI.java
@@ -8,14 +8,24 @@ import org.springframework.web.bind.annotation.*;
 import tn.esprit.cosharemsmouhib.entities.Product;
 import tn.esprit.cosharemsmouhib.services.ProductService;
 
+import javax.annotation.security.RolesAllowed;
+
 @RestController
 public class ProductRestAPI {
     private String title="hello , im the updated product microservice";
+    private String admin="hello , im admin";
 
     @RequestMapping("/hello")
+    @RolesAllowed("user")
     public String sayHello(){
         System.out.println(title);
         return title;
+    }
+    @RequestMapping("/admin")
+    @RolesAllowed("admin")
+    public String sayAdmin(){
+        System.out.println(admin);
+        return admin;
     }
     @Autowired
     private ProductService productService;
